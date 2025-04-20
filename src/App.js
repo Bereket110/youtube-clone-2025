2167a33e0488ca6ace721eeff0a588ac4ce74d11
Bereket110 <bereketyakob93@gmail.com>
2025-04-20 23:30:54 +0300

    Header

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./Components";
+
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
