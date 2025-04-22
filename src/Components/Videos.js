1363c471c174ea9f4238ec8fa75f78ee536d77b4
Bereket110 <bereketyakob93@gmail.com>
2025-04-22 11:14:40 +0300

    Video render to front end

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -6,7 +6,7 @@ const Videos = ({ videos }) => {
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
-          {item.snippet.channelId && <ChannelCard channelDetail={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
