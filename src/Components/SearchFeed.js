7fd70bdbe9c06d35054d51968df9708193d3221a
Bereket110 <bereketyakob93@gmail.com>
2025-04-24 12:14:38 +0300

    Channel and videos

--- src/Components/SearchFeed.js
+++ src/Components/SearchFeed.js
@@ -1,7 +1,29 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Videos } from "./";
 
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const { searchTerm } = useParams();
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    // setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+  return (
+    <Box p={2} sx={{ overflow: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        search Results is:{" "}
+        <span style={{ color: "#F31503" }}>{searchTerm}</span> Videos
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
